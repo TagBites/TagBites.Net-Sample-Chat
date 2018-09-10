463b40b6b5d81a6f6c71f0dd88bac33fc575d384
Tomasz Malik <tomasz.malik@outlook.com>
2018-09-10 15:31:00 +0200

    First version

---
+++ src/Chat.ClientApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagBites.Net;
+
+namespace Chat.ClientApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new Client("127.0.0.1", 82);
+            client.Received += (s, e) => Console.WriteLine(e.Message.ToString());
+            await client.ConnectAsync();
+
+            while (true)
+                await client.SendAsync(Console.ReadLine());
+        }
+    }
+}
---
+++ src/Chat.ServerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TagBites.Net;
+
+namespace Chat.ServerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server("127.0.0.1", 82);
+            server.Received += async (s, e) => await server.SendToAllAsync($"{e.Client}: {e.Message}", e.Client);
+            server.ClientConnected += async (s, e) => await server.SendToAllAsync($"{e.Client} connected", e.Client);
+            server.ClientDisconnected += async (s, e) => await server.SendToAllAsync($"{e.Client} disconnected", e.Client);
+            server.Listening = true;
+
+            Console.ReadLine();
+        }
+    }
+}
